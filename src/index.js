c7a953e039652ef3b14d76b52c9733861faeaf46
Uday207 <chinthamanuudaykumar@gmail.com>
2025-03-14 19:43:54 +0530

    Fixed missing react-redux dependency

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { PersistGate } from 'redux-persist/integration/react';
-import "slick-carousel/slick/slick.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import "./index.css";
 import App from "./App";
+import "slick-carousel/slick/slick.css?inline";
+import "slick-carousel/slick/slick-theme.css?inline";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
